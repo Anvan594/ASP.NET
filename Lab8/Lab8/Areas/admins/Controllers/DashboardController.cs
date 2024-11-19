ffa180c33abcad0403e916df577c6c0f0da1901d
Nguyễn Văn An <vanan9524@gmail.com>
2024-11-19 22:27:20 +0700

    thứ  19/11/2024

---
+++ Lab8/Lab8/Areas/admins/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab8.Areas.admins.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("admins")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
